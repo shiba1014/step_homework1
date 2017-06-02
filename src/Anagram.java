6ccc4e0b6cf99b619f2091f76ce89cb97c67d786
shiba1014 <shiba.png@gmail.com>
2017-06-02 12:21:11 +0900

    get high score

--- src/Anagram.java
+++ src/Anagram.java
@@ -3,19 +3,27 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.attribute.AclEntry.Builder;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Anagram {
 	private static BufferedReader br;
-	private static Scanner sc;
-	static boolean isRun = true;
-
+	private static Scanner sc = new Scanner(System.in);;
+	static String[] points1 = {"a","b","d","e","g","i","n","o","r","s","t","u"};
+	static String[] points2 = {"c","f","h","l","m","p","v","w","y"};
+	static String[] points3 = {"j","k","q","x","z"};
+	
+	static String answerStr = "";
+	static Integer maxPoint = 0;
+	static String word;
+	
 	public static void main(String[] args) {
-		while (isRun) {
-			sc = new Scanner(System.in);
-			String searchString = sc.next();
+		while (true) {
+			String searchString = sc.next().toLowerCase();
 			if (searchString.equals("Q")) {
-				isRun = false;
 				break;
 			}
 			try {
@@ -23,29 +31,51 @@ public class Anagram {
 				FileReader fileReader = new FileReader(file);
 				br = new BufferedReader(fileReader);
 				
-				String word = br.readLine();
+				word = br.readLine().toLowerCase();
 				while (word != null) {
 					if (word.length() <= 2) {
 						word = br.readLine();
 						continue;
 					}
+					
 					StringBuilder builder = new StringBuilder(searchString);
+					Integer point = 1;
 					for (int i = 0; i < word.length(); i++) {
-						Integer index = builder.indexOf(String.valueOf(word.charAt(i)));
+//						if (word.equals("atrocity")) {
+//							System.out.println(word);
+//							System.out.println("length is");
+//							System.out.println(word.length());
+//						}
+						String searchChar = String.valueOf(word.charAt(i));
+						Integer index = builder.indexOf(searchChar);
 						if (index == -1) {
-							word = br.readLine();
+//							word = br.readLine();
 							break;
 						} else {
+							if (Arrays.asList(points1).contains(searchChar)) {
+								point += 1;
+							} else if (Arrays.asList(points2).contains(searchChar)) {
+								point += 2;
+							} else if (Arrays.asList(points3).contains(searchChar)) {
+								point += 3;
+							}
 							builder.deleteCharAt(index);
-							if (i == word.length() - 1) {
-								System.out.println(word);
-								word = br.readLine();
+							if (i == (word.length() - 1)) {
+								point *= point;
+								if (point > maxPoint) {
+									answerStr = word;
+									maxPoint = point;
+								}
 							}
 						}
 					}
+					word = br.readLine();
 				}
 				
+				System.out.println(answerStr);
+				System.out.println(maxPoint);
 				System.out.println("finish");
+				searchString = "";
 				
 			} catch (FileNotFoundException e) {
 				System.out.println(e);
